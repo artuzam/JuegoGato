18fd7d1a1d955424e2143bc8d1381d273426d5be
artuzam <charliecazc@gmail.com>
2015-07-30 17:49:34 -0600

    Métodos del tablero
    Se añaden los métodos que inicializan y modifican el tablero

--- Gato.java
+++ Gato.java
@@ -14,5 +14,28 @@ public class Gato{
   public String getNombreO(){
     return jugador2;
   }
-
+  public void iniciarTablero(){
+    for (int i=0; i < tablero.length; i++) {
+      for (int j=0; j < tablero[i].length; j++) {
+        tablero[i][j] = " ";
+      }
+    }
+    for (int i=0; i < matriz.length; i++) {
+      for (int j=0; j < matriz[i].length; j++) {
+        matriz[i][j] = 0;
+      }
+    }
+  }
+  public boolean modificarTablero(int jugador)
+  {
+    boolean d= false;
+    if(matriz[fila][columna]==0)
+    {
+      matriz[fila][columna]=jugador;
+      d= true;
+    }
+    return d;
+  }
+  
+ 
 }
